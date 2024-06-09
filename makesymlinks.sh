6ea40bc4df99844f9a12e6d8ca6498c6942c960a
Andrii Shevtsov <andrii.shevtsov@outlook.com>
2024-06-09 14:36:39 +0300

    AS: changed script to sync oh-my-zsh

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -25,7 +25,7 @@ echo "done"
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
+    mv ~/.$file ~/$olddir/
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
@@ -35,7 +35,7 @@ install_zsh () {
 if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
     # Clone my oh-my-zsh repository from GitHub only if it isn't already present
     if [[ ! -d $dir/oh-my-zsh/ ]]; then
-        git clone https://github.com/sheffdon/dotfiles.git
+        git clone https://github.com/ohmyzsh/ohmyzsh.git
     fi
     # Set the default shell to zsh if it isn't currently set to zsh
     if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
